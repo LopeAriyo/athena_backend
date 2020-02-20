82e1a07be427dbf4aa5d3a3b64ac59a4acfce9f7
Lope Ariyo <lopeariyo@gmail.com>
2020-02-20 09:40:25 +0000

    inital commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  post '/signin', to: 'users#signin'
+  post '/signup', to: 'users#signup'
+  get '/validate', to: 'users#validate'
+  # get '/cycle', to: 'users#cycle'
+  # get '/journal', to: 'users#journal'
+  # get '/insights', to: 'users#insights'
+  # get '/profile', to: 'users#profile'
+end
